3836a49b69d860805ab793ab6da952d204bfb918
EnzoMessaoudi <12168@holbertonstudents.com>
2026-05-20 12:56:50 +0200

    ADD paprec Logo to the top right of the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,26 @@
+import icon from "/Paprec_Logo.png";
+
 export default function Header({ title }) {
   return (
-    <div className="bg-[#003B71] text-white p-6 rounded-2xl shadow mb-6">
-      <h1 className="text-3xl md:text-4xl font-bold">{title}</h1>
+    <div className="relative bg-[#003B71] text-white p-6 rounded-2xl shadow mb-6">
+
+      <div className="absolute right-7 top-1/2 -translate-y-1/2 flex flex-col items-center">
+        
+        <img
+          src={icon}
+          alt="Paprec Logo"
+          className="h-14 object-contain "
+        />
+
+        <p className="-mt-4 text-1xl font-bold -ml-11">
+          Sens
+        </p>
+
+      </div>
+
+      <h1 className="text-3xl md:text-4xl font-bold">
+        {title}
+      </h1>
     </div>
   );
 }
