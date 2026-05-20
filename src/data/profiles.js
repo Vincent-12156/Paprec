53d5fb22317b0e27369da9488ea946e63a93d657
EnzoMessaoudi <12168@holbertonstudents.com>
2026-05-20 13:06:09 +0200

    ADD new persons

--- src/data/profiles.js
+++ src/data/profiles.js
@@ -23,4 +23,100 @@ export const profiles = [
     lastName: "Nowak",
     company: "PolCargo",
   },
-];
+  {
+    id: 5,
+    firstName: "Jean",
+    lastName: "Lemoine",
+    company: "DataCorp"
+  },
+  {
+    id: 6,
+    firstName: "Emma",
+    lastName: "Durand",
+    company: "TechNova"
+  },
+  {
+    id: 7,
+    firstName: "Louis",
+    lastName: "Moreau",
+    company: "Buildix"
+  },
+  {
+    id: 8,
+    firstName: "Camille",
+    lastName: "Dupont",
+    company: "GreenSoft"
+  },
+  {
+    id: 9,
+    firstName: "Nathan",
+    lastName: "Roux",
+    company: "TransLog"
+  },
+  {
+    id: 10,
+    firstName: "Julie",
+    lastName: "Faure",
+    company: "Buildix"
+  },
+  {
+    id: 11,
+    firstName: "Hugo",
+    lastName: "Dupuis",
+    company: "DataCorp"
+  },
+  {
+    id: 12,
+    firstName: "Marie",
+    lastName: "Blanc",
+    company: "NextVision"
+  },
+  {
+    id: 13,
+    firstName: "Clara",
+    lastName: "Bernard",
+    company: "TechNova"
+  },
+  {
+    id: 14,
+    firstName: "Antoine",
+    lastName: "Garnier",
+    company: "TransLog"
+  },
+  {
+    id: 15,
+    firstName: "Lucie",
+    lastName: "Chevalier",
+    company: "NextVision"
+  },
+  {
+    id: 16,
+    firstName: "Thomas",
+    lastName: "Petit",
+    company: "GreenSoft"
+  },
+  {
+    id: 17,
+    firstName: "Emma",
+    lastName: "Dupont",
+    company: "Buildix"
+  },
+  {
+    id: 18,
+    firstName: "Paul",
+    lastName: "Renaud",
+    company: "DataCorp"
+  },
+  {
+    id: 19,
+    firstName: "Julie",
+    lastName: "Martin",
+    company: "TechNova"
+  },
+  {
+    id: 20,
+    firstName: "Alexandre",
+    lastName: "Lefevre",
+    company: "TransLog"
+  }
+]
